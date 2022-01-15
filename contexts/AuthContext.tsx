88d78108053aaa81f120bf2c9bdac1ff7513054c
msalves008 <matheusdesousaalves2018@gmail.com>
2022-01-15 04:19:22 -0300

    make logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies' 
-import Router from 'next/router'
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+import Router from "next/router";
 
 import { api } from "../services/api";
 
@@ -13,7 +13,7 @@ type User = {
 type SignInCredentials = {
   email: string;
   password: string;
-}
+};
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
@@ -23,54 +23,65 @@ type AuthContextData = {
 
 type AuthProviderProps = {
   children: ReactNode;
-}
+};
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
+
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+  Router.push("/");
+}
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
   useEffect(() => {
-    const { 'nextauth.token': token } = parseCookies()
+    const { "nextauth.token": token } = parseCookies();
 
     if (token) {
-      api.get('/me').then(response => {
-        const { email, permissions, roles } = response.data
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles })
-      })
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
     }
-  }, [])
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post('sessions', {
+      const response = await api.post("sessions", {
         email,
         password,
-      })
+      });
 
       const { token, refreshToken, permissions, roles } = response.data;
 
-      setCookie(undefined, 'nextauth.token', token, {
+      setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
+        path: "/",
+      });
 
-      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
+        path: "/",
+      });
 
       setUser({
         email,
         permissions,
         roles,
-      })
+      });
 
-      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
-      Router.push('/dashboard');
+      Router.push("/dashboard");
     } catch (err) {
       console.log(err);
     }
@@ -80,5 +91,5 @@ export function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,6 +5,7 @@ export default function Dashboard() {
   const { user } = useContext(AuthContext);
   useEffect(()=>{
     api.get('/me').then(response=> console.log(response))
+    .catch(err => console.log(err))
   },[])
   return (
       <h1>Dashboard:  {user?.email}</h1>
