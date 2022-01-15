7716705f68b5e987e20d8ff5e7453ad742350450
msalves008 <matheusdesousaalves2018@gmail.com>
2022-01-14 21:11:54 -0300

    FIX: Resolve Bug can't read propety response.data at AuthContext

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from "nookies";
-import Router from "next/router";
+import { setCookie, parseCookies } from 'nookies' 
+import Router from 'next/router'
+
 import { api } from "../services/api";
-import { HeadersDefaults } from "axios";
 
 type User = {
   email: string;
@@ -13,69 +13,72 @@ type User = {
 type SignInCredentials = {
   email: string;
   password: string;
-};
+}
 
 type AuthContextData = {
-  signIn: (credentials: SignInCredentials) => Promise<void>;
-  user: User | undefined;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
-};
+}
 
-export const AuthContext = createContext({} as AuthContextData);
+export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
   useEffect(() => {
-    const { "nextauth.token": token } = parseCookies();
+    const { 'nextauth.token': token } = parseCookies()
+
     if (token) {
-      api.get("/me").then((response) => {
-        const { email, permissions, roles } = response.data;
-        setUser({ email, permissions, roles });
-      });
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({ email, permissions, roles })
+      })
     }
-  }, []);
+  }, [])
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post("sessions", {
+      const response = await api.post('sessions', {
         email,
         password,
-      });
+      })
+
       const { token, refreshToken, permissions, roles } = response.data;
 
-      setCookie(undefined, "nextauth.token", token, {
+      setCookie(undefined, 'nextauth.token', token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: "/",
-      });
-      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: "/",
-      });
+        path: '/'
+      })
 
-      setUser({ email, permissions, roles });
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
 
-      // adiciona o token nas configurações default do axios na primeira requisição
-      interface CommonHeaderProperties extends HeadersDefaults {
-        Authorization: string;
-      }
-      api.defaults.headers = {
-        Authorization: `Bearer ${token}`,
-      } as CommonHeaderProperties;
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
-      Router.push("/dashboard");
-    } catch (error) {
-      console.log(error);
+      Router.push('/dashboard');
+    } catch (err) {
+      console.log(err);
     }
   }
+
   return (
     <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
